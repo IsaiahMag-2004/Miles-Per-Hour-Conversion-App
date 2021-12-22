ad8b56a1efa2db35704458ba03b6944d88980e91
Isaiah Magana <Maganaisaiah4@gmail.com>
2021-12-22 12:23:14 -0500

    Created program

---
+++ Main.py
@@ -0,0 +1,15 @@
+#Unit 1 Challanger 2 Miles Per Hour Converter App
+
+print("Welcome to the Miles Per Hour Conversion App")
+print()
+
+#Get speed
+miles_per_hour = input("What is your speed in miles per hour: ")
+
+#Convert and round
+kilometers = miles_per_hour / 2.237
+kilometers = round(kilometers, 2)
+
+#Cast, and output
+kilometers = str(kilometers)
+print("Your speed in meters per second is " + kilometers + ".")
